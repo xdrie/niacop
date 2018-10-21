ce3d84182287308b847774e88f20745170b4163d
xdrie <xdrie@alt.icu>
2018-10-21 10:47:25 -0700

    parse error handling

--- src/niacop/niacop/Native/WindowManagers/XWindowManager.cs
+++ src/niacop/niacop/Native/WindowManagers/XWindowManager.cs
@@ -1,4 +1,7 @@
-﻿namespace niacop.Native.WindowManagers {
+﻿using System;
+using niacop.Services;
+
+namespace niacop.Native.WindowManagers {
     public class XWindowManager : IWindowManager {
         public int getIdleTime() {
             // xprintidle
@@ -7,18 +10,23 @@
         }
 
         public Window getActiveWindow() {
+            // TODO: proper error handling for missing xdotool
             var wClass = Shell.executeShellCommand("xprop",
                 "-id $(xdotool getwindowfocus) WM_CLASS | cut -d \'\"\' -f4");
             var wTitle = Shell.executeShellCommand("xprop",
                 "-id $(xdotool getwindowfocus) _NET_WM_NAME | cut -d \'\"\' -f2");
             var wPid = Shell.executeShellCommand("xprop",
                 "-id $(xdotool getwindowfocus) _NET_WM_PID | cut -d \' \' -f3");
-            // TODO: proper error handling for missing xdotool
-            return new Window {
-                application = wClass.output,
-                processId = int.Parse(wPid.output),
-                title = wTitle.output
-            };
+            try {
+                return new Window {
+                    application = wClass.output,
+                    processId = int.Parse(wPid.output),
+                    title = wTitle.output
+                };
+            } catch (FormatException e) {
+                Logger.log($"format exception: {wPid} - {e}", Logger.Level.Error);
+                throw;
+            }
         }
     }
 }
