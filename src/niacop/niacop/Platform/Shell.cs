72814839903d77ca843d14a4ff61c0784381f6d5
xdrie <xdrie@alt.icu>
2018-10-20 19:25:54 -0700

    create project

---
+++ src/niacop/niacop/Platform/Shell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace niacop.Platform {
+    public static class Shell {
+        public enum ShellExecutorType
+        {
+            Generic,
+            Windows,
+            Unix
+        }
+        
+        private static ShellExecutorType executorType;
+        
+        static Shell()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                    executorType = ShellExecutorType.Windows;
+                    break;
+
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                    executorType = ShellExecutorType.Unix;
+                    break;
+            }
+        }
+    }
+}
---
+++ src/niacop/niacop/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace niacop {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("[niacop]");
+        }
+    }
+}
