86d1cdefa41d254d21579799a4df274b3a98d12e
xdrie <xdrie@alt.icu>
2018-10-29 21:37:55 -0700

    properly handle days

--- src/niaview/app.js
+++ src/niaview/app.js
@@ -36,13 +36,15 @@ function msToTime(duration) {
   var milliseconds = parseInt((duration % 1000) / 100),
     seconds = parseInt((duration / 1000) % 60),
     minutes = parseInt((duration / (1000 * 60)) % 60),
-    hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+    days = parseInt((duration / (1000 * 60 * 60 * 24)));
 
+  days = (days < 10) ? "0" + days : days;
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  return days + ":" + hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
 function onActivityUploaded(files) {
