759f318a516ce80068109ac7b3907883deca812d
xdrie <xdrie@alt.icu>
2020-05-23 15:53:51 -0700

    add comments for stackoverflow links

--- src/niacop/Nia/Native/Platforms/WinApiWindowManager.cs
+++ src/niacop/Nia/Native/Platforms/WinApiWindowManager.cs
@@ -12,14 +12,18 @@ namespace Nia.Native.WindowManagers {
 
         public int getIdleTime() {
             // GetLastInputInfo
+            // https://stackoverflow.com/a/11901730/13240621
             throw new NotImplementedException();
         }
 
         public Window getActiveWindow() {
+            // GetForegroundWindow
+            // https://stackoverflow.com/a/115905/13240621
             throw new NotImplementedException();
         }
 
         public void hookUserEvents(Action<KeyboardEvent> callback) {
+            // https://stackoverflow.com/a/604417/13240621
             throw new NotImplementedException();
         }
     }
