ee14c1ebe8f4327d3b17df4b756076d9566d35f0
xdrie <xdrie@alt.icu>
2020-05-22 18:07:56 -0700

    create default log sink

--- src/niacop/niacop/Global.cs
+++ src/niacop/niacop/Global.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using Iri.Glass.Logging;
 using niacop.Configuration;
 
 namespace niacop {
     public static class Global {
         public static Config config;
-        public static Logger log = new Logger(Logger.Verbosity.Information);
+        public static Logger log = new Logger(Logger.Verbosity.Information) {
+            sinks = new List<Logger.ILogSink> {
+                new Logger.ConsoleSink()
+            }
+        };
     }
 }
