8c70b5310fb40259d13bf096502bd2ff59cc18e0
xdrie <xdrie@alt.icu>
2021-01-13 01:02:03 -0800

    fix summary when no results

--- src/niacop/Nia/CLI/TimeMachineRunner.cs
+++ src/niacop/Nia/CLI/TimeMachineRunner.cs
@@ -16,10 +16,12 @@ namespace Nia.CLI {
             [Value(0, Required = true, MetaName = "query", HelpText = "Time query.")]
             public string? timeQuery { get; set; }
 
-            [Option('p', "period", Required = false, HelpText = "The period of time (in hours) to show usage summary for.")]
+            [Option('p', "period", Required = false,
+                HelpText = "The period of time (in hours) to show usage summary for.")]
             public float period { get; set; } = 1f; // hours
 
-            [Option('r', "range", Required = false, HelpText = "The time range (in hours) to look for sessions in for rough-matching.")]
+            [Option('r', "range", Required = false,
+                HelpText = "The time range (in hours) to look for sessions in for rough-matching.")]
             public float roughMatchRange { get; set; } = 24f; // hours
 
             [Option('n', "entries", Required = false, HelpText = "The number of top entries in the usage summary.")]
@@ -61,8 +63,7 @@ namespace Nia.CLI {
                 Global.log.info($"found {matchedSessions.Count} precise match.");
                 // print the session nicely
                 Global.log.info($"\n{preciseSess.prettyFormat()}");
-            }
-            else {
+            } else {
                 // find close matches
                 Global.log.info($"no precise matches found.");
                 // find session immediately before and after (within range)
@@ -84,16 +85,14 @@ namespace Nia.CLI {
                 if (immBefore == null && immAfter == null) {
                     Global.log.info(
                         $"no sessions found within {options.roughMatchRange:F0} hours of requested time.");
-                }
-                else {
+                } else {
                     var beforeDist = Math.Abs((queryTimestamp - immBefore?.endTime) ?? long.MaxValue);
                     var afterDist = Math.Abs((queryTimestamp - immAfter?.startTime) ?? long.MaxValue);
                     // pick the closer one
                     if (beforeDist <= afterDist) {
                         closest = immBefore;
                         closestDist = beforeDist;
-                    }
-                    else {
+                    } else {
                         closest = immAfter;
                         closestDist = afterDist;
                     }
@@ -129,25 +128,27 @@ namespace Nia.CLI {
 
             // get the top-N usages
             var topUsages = usages.Values.OrderByDescending(x => x.time)
-                .Take(options.topN);
+                .Take(options.topN).ToList();
 
-            var sb = new StringBuilder();
-            sb.AppendLine(
-                $"usage summary (top {options.topN} within {options.period:F0}h) from {periodStartDate.DateTime:HH:mm}-{periodEndDate.DateTime:HH:mm}");
-            foreach (var usage in topUsages) {
-                var humanTime = TimeSpan.FromMilliseconds(usage.time);
+            if (topUsages.Any()) {
+                var sb = new StringBuilder();
                 sb.AppendLine(
-                    $"{usage.application,-32} {humanTime,6:mm\\:ss} // {Utils.formatNumberSI(usage.keyEvents),8:F0} keys");
-            }
+                    $"usage summary (top {options.topN} within {options.period:F0}h) from {periodStartDate.DateTime:HH:mm}-{periodEndDate.DateTime:HH:mm}");
+                foreach (var usage in topUsages) {
+                    var humanTime = TimeSpan.FromMilliseconds(usage.time);
+                    sb.AppendLine(
+                        $"{usage.application,-32} {humanTime,6:mm\\:ss} // {Utils.formatNumberSI(usage.keyEvents),8:F0} keys");
+                }
 
-            Global.log.info(sb.ToString());
-            
-            var summarizer = new SessionRangeSummarizer(tracker);
-            var (tagResult, graphData) = summarizer.summarizeSessionRange(periodStartDate, periodEndDate);
-            
-            summarizer.printSummaryReport(graphData,
-                $"{(int) options.period} HOURS // {tagResult.SessionCount} SESSIONS // TOTAL {FormatHelper.formatTimeHM(tagResult.TotalTime)}",
-                "TIMEMACHINE MODE");
+                Global.log.info(sb.ToString());
+
+                var summarizer = new SessionRangeSummarizer(tracker);
+                var (tagResult, graphData) = summarizer.summarizeSessionRange(periodStartDate, periodEndDate);
+
+                summarizer.printSummaryReport(graphData,
+                    $"{(int) options.period} HOURS // {tagResult.SessionCount} SESSIONS // TOTAL {FormatHelper.formatTimeHM(tagResult.TotalTime)}",
+                    "TIMEMACHINE MODE");
+            }
 
             return 0;
         }
